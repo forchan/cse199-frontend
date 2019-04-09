7f2b096c977c307f0487aa64b007ce63059da712
forchan <conloonc@buffalo.edu>
2019-04-09 04:05:28 -0400

    refactor announcements component

--- src/components/Announcements.jsx
+++ src/components/Announcements.jsx
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import {
   TabContent,
   TabPane,
@@ -11,65 +12,61 @@ import classnames from 'classnames';
 import AnnouncementCard from './Cards/AnnouncementCard.jsx';
 import SendAnnouncementModal from './Modals/SendAnnouncementModal.jsx';
 
-class Announcements extends Component {
-  state = {
-    sendAnnouncementModal: false,
-    activeTab: '1'
-  }
-  toggleModal = () => {
-    this.setState(prevState => ({
-      sendAnnouncementModal: !prevState.sendAnnouncementModal
-    }));
-  }
-  toggleTab = (tab) => {
-    if (this.state.activeTab !== tab) {
-      this.setState({
-        activeTab: tab
-      });
+const propTypes = {
+  announcements: PropTypes.array.isRequired
+};
+
+const Announcements = ({ announcements }) => {
+  const [sendAnnouncementModal, setModal] = useState(false);
+  const [activeTab, setTab] = useState('1');
+  const toggleModal = () => setModal(!sendAnnouncementModal);
+  const toggleTab = tab => {
+    if (activeTab !== tab) {
+      setTab(tab);
     }
   }
-  render() {
-    return (
-      <div className="content">
-        <SendAnnouncementModal
-          isOpen={this.state.sendAnnouncementModal}
-          toggle={this.toggleModal}
-        />
-        <Nav tabs>
-          <NavItem>
-            <NavLink
-              className={classnames({ active: this.state.activeTab === '1' })}
-              onClick={() => { this.toggleTab('1'); }}
-            >
-              <span id="PreviousTooltip" href="#">Previous</span>
-              <UncontrolledTooltip placement="top" target="PreviousTooltip">
-                Filtered by most recent
-              </UncontrolledTooltip>
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink onClick={() => { this.toggleModal(); }}>
-              Send New{' '}
-              <span data-notify="icon" className="nc-icon nc-send" />
-            </NavLink>
-          </NavItem>
-        </Nav>
-        &nbsp;
-        <TabContent activeTab={this.state.activeTab}>
-          <TabPane tabId="1">
-            {this.props.announcements.map((announcement, index) => {
-              return (
-                <AnnouncementCard announcement={announcement} key={index} />
-              )
-            })}
-          </TabPane>
-          <TabPane tabId="2">
-            This wont't show lol.
-          </TabPane>
-        </TabContent>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="content">
+      <SendAnnouncementModal
+        isOpen={sendAnnouncementModal}
+        toggle={toggleModal}
+      />
+      <Nav tabs>
+        <NavItem>
+          <NavLink
+            className={classnames({ active: activeTab === '1' })}
+            onClick={() => toggleTab('1')}
+          >
+            <span id="PreviousTooltip" href="#">Previous</span>
+            <UncontrolledTooltip placement="top" target="PreviousTooltip">
+              Filtered by most recent
+            </UncontrolledTooltip>
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink onClick={() => toggleModal()}>
+            Send New{' '}
+            <span data-notify="icon" className="nc-icon nc-send" />
+          </NavLink>
+        </NavItem>
+      </Nav>
+      &nbsp;
+      <TabContent activeTab={activeTab}>
+        <TabPane tabId="1">
+          {announcements.map((announcement, index) => {
+            return (
+              <AnnouncementCard announcement={announcement} key={index} />
+            )
+          })}
+        </TabPane>
+        <TabPane tabId="2">
+          This wont't show lol.
+        </TabPane>
+      </TabContent>
+    </div>
+  );
+};
+
+Announcements.propTypes = propTypes;
 
 export default Announcements;
