e775ccb533bffe3e1a0b21246e0fe3bfe7374c56
forchan <conloonc@buffalo.edu>
2019-04-17 02:56:13 -0400

    getStuff to getApiStuff

--- src/utils/ApiHelperUtils.js
+++ src/utils/ApiHelperUtils.js
@@ -4,7 +4,7 @@
  *
  * Therefore, these helper functions assist ApiUtils.js, mainly for GET calls
  */
-import { getStuff } from './ApiUtils.js';
+import { getApiStuff } from './ApiUtils.js';
 import {
   GET_ANNOUNCEMENTS,
   GET_ACTIVITIES,
@@ -19,22 +19,22 @@ import {
 } from '../constants/ApiConstants.js';
 
 export const loadActivities = async (courseId) => {
-  let { data } = await getStuff({ action: GET_ACTIVITIES, courseId: courseId });
+  let { data } = await getApiStuff({ action: GET_ACTIVITIES, courseId: courseId });
   return (data) ? data.activities : [];
 }
 
 export const loadAnnouncements = async (courseId) => {
-  let { data } = await getStuff({ action: GET_ANNOUNCEMENTS, courseId: courseId });
+  let { data } = await getApiStuff({ action: GET_ANNOUNCEMENTS, courseId: courseId });
   return (data) ? data.announcements.reverse() : []; // most recent first
 }
 
 export const loadAssignments = async (courseId) => {
-  let { data } = await getStuff({ action: GET_ASSIGNMENTS, courseId: courseId });
+  let { data } = await getApiStuff({ action: GET_ASSIGNMENTS, courseId: courseId });
   return (data) ? data.assignments : [];
 }
 
 export const loadCalendar = async (courseId) => {
-  const { data } = await getStuff({ action: GET_CALENDAR, courseId: courseId });
+  const { data } = await getApiStuff({ action: GET_CALENDAR, courseId: courseId });
   let calendar = [];
   if (data) {
     for (let num = 1; num <= 7; num++) { // need to extract block values from json
@@ -46,31 +46,31 @@ export const loadCalendar = async (courseId) => {
 }
 
 export const loadInstructors = async () => {
-  let { data } = await getStuff({ action: GET_INSTRUCTOR_LIST });
+  let { data } = await getApiStuff({ action: GET_INSTRUCTOR_LIST });
   return (data) ? data.instructors : [];
 }
 
 export const loadLectureNotes = async (courseId) => {
-  let { data } = await getStuff({ action: GET_LECTURE_NOTES, courseId: courseId });
+  let { data } = await getApiStuff({ action: GET_LECTURE_NOTES, courseId: courseId });
   return (data) ? data.lectureNotes : [];
 }
 
 export const loadModules = async (courseId) => {
-  const { data } = await getStuff({ action: GET_MODULES, courseId: courseId });
+  const { data } = await getApiStuff({ action: GET_MODULES, courseId: courseId });
   return (data) ? data.modules : [];
 }
 
 export const loadOfficeHours = async (courseId) => {
-  let { data } = await getStuff({ action: GET_OFFICE_HOURS, courseId: courseId });
+  let { data } = await getApiStuff({ action: GET_OFFICE_HOURS, courseId: courseId });
   return (data) ? data.officehours : [];
 }
 
 export const loadSections = async (courseId) => {
-  const { data } = await getStuff({ action: GET_COURSE_AND_SECTIONS, courseId: courseId });
+  const { data } = await getApiStuff({ action: GET_COURSE_AND_SECTIONS, courseId: courseId });
   return (data) ? data.sections : [];
 }
 
 export const loadSectionGroups = async (courseId) => {
-  const { data } = await getStuff({ action: GET_SECTION_GROUPS, courseId: courseId });
+  const { data } = await getApiStuff({ action: GET_SECTION_GROUPS, courseId: courseId });
   return (data) ? data.section_groups : [];
 }
--- src/utils/ApiUtils.js
+++ src/utils/ApiUtils.js
@@ -2,16 +2,15 @@ import axios from 'axios';
 import { API_VIEW_URL } from '../constants/ApiConstants.js';
 
 // main function for making api get calls
-export const getStuff = async (params) => {
+export const getApiStuff = async (params) => {
   try {
     return await axios.get(API_VIEW_URL, { params: params });
   } catch (error) {
     return error;
   }
-}
+};
 
-// main function for making post calls
-// returns a string that describes success or error
+// main function for making post calls, returns string with success or error
 export const postApiStuff = async (url, params) => {
   try {
     let { data } = await axios.post(url, JSON.stringify(params));
@@ -19,4 +18,4 @@ export const postApiStuff = async (url, params) => {
   } catch (error) {
     return `${error}`; // template literal converts error from some weird object to string
   }
-}
+};
