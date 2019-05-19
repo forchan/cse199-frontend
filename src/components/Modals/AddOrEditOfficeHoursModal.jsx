518044230c9044a0bc196f0c441c0f26a1487674
forchan <conloonc@buffalo.edu>
2019-05-19 06:06:26 -0400

    add/edit office hour form

--- src/components/Modals/AddOrEditOfficeHoursModal.jsx
+++ src/components/Modals/AddOrEditOfficeHoursModal.jsx
@@ -6,12 +6,14 @@ import {
   ModalBody,
   ModalFooter,
   Col,
+  Row,
   Form,
   FormGroup,
   Label,
   Input,
   Button
 } from 'reactstrap';
+import { REG_OFFICE_HOUR_TYPE } from '../../constants/InstructorConstants.js';
 
 const propTypes = {
   isOpen:  PropTypes.bool.isRequired,
@@ -22,13 +24,18 @@ const AddOrEditOfficeHoursModal = ({
   isOpen,
   toggle
 }) => {
+  const [weekday, setWeekday] = useState('');
+  const [location, setLocation] = useState('');
+  const [officeHourType, setOfficeHourType] = useState(REG_OFFICE_HOUR_TYPE);
+  const [startTime, setStartTime] = useState('');
+  const [endTime, setEndTime] = useState('');
 
   const validForm = () => {
-
+    return true;
   };
 
   const submitForm = async () => {
-
+    if (!validForm()) return;
   };
 
   return (
@@ -37,10 +44,76 @@ const AddOrEditOfficeHoursModal = ({
         Office Hours
       </ModalHeader>
       <ModalBody style={{ height: 'auto' }}>
-
+        <Form>
+          <Row form>
+            <Col sm={6}>
+              <FormGroup>
+                <Label for="weekday">Weekday* [ex. M or T-R]</Label>
+                <Input
+                  type="text"
+                  name="weekday"
+                  id="weekday"
+                  value={weekday}
+                  onChange={e => setWeekday(e.target.value)}
+                />
+              </FormGroup>
+            </Col>
+            <Col md={6}>
+              <FormGroup>
+                <Label for="location">Location*</Label>
+                <Input
+                  type="text"
+                  name="location"
+                  id="location"
+                  value={location}
+                  onChange={e => setLocation(e.target.value)}
+                />
+              </FormGroup>
+            </Col>
+          </Row>
+          <Row form>
+            <Col sm={4}>
+              <FormGroup>
+                <Label for="type">Type</Label>
+                <Input
+                  type="text"
+                  name="type"
+                  id="type"
+                  disabled
+                  value={officeHourType}
+                  onChange={e => setOfficeHourType(e.target.value)}
+                />
+              </FormGroup>
+            </Col>
+            <Col sm={4}>
+              <FormGroup>
+                <Label for="startTime">Start time*</Label>
+                <Input
+                  type="time"
+                  name="startTime"
+                  id="startTime"
+                  value={startTime}
+                  onChange={e => setStartTime(e.target.value)}
+                />
+              </FormGroup>
+            </Col>
+            <Col md={4}>
+              <FormGroup>
+                <Label for="endTime">End time*</Label>
+                <Input
+                  type="time"
+                  name="endTime"
+                  id="endTime"
+                  value={endTime}
+                  onChange={e => setEndTime(e.target.value)}
+                />
+              </FormGroup>
+            </Col>
+          </Row>
+        </Form>
       </ModalBody>
       <ModalFooter>
-        <Button color="primary" onClick={toggle}>Add</Button>{' '}
+        <Button color="primary" onClick={submitForm}>Add</Button>{' '}
         <Button color="secondary" onClick={toggle}>Cancel</Button>
       </ModalFooter>
     </Modal>
--- src/constants/InstructorConstants.js
+++ src/constants/InstructorConstants.js
@@ -3,3 +3,5 @@ import { BASE_URL } from './ApiConstants.js';
 export const DEFAULT_IMAGE_PATH = BASE_URL + '/images/default.jpg';
 export const INSTRUCTOR = 'INSTRUCTOR';
 export const TA = 'TA';
+
+export const REG_OFFICE_HOUR_TYPE = 'REG';
